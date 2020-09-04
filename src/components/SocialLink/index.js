688b29daff5911e43e692a33a8dd3ceee4dc18ef
GNG <GNG@Guilhermes-MacBook-Pro.local>
2020-09-04 13:28:25 -0300

    fix errors

--- src/components/SocialLink/index.js
+++ src/components/SocialLink/index.js
@@ -14,7 +14,7 @@ import './styles.css';
 
 function SocialLink({ provider }) {
   return (
-    <a href={`https://accounts.evaluatz.com/connect/${provider}`} className="link">
+    <a href={`https://db.evaluatz.herokuapp.com/connect/${provider}`} className="link">
       <Button type="button" className={`btn ${provider}`} social={provider} style={{ width: '100%' }}>
         <i className={`fab fa-${provider}`} style={{ marginRight: '10px' }} />
         {capitalize(provider)}
